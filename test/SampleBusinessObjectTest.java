17561b3b0150cc287103a3f5d89241d957cc28e2
543e-crypto <147545942+543e-crypto@users.noreply.github.com>
2023-10-20 00:29:00 -0400

    Update SampleBusinessObjectTest.java

--- test/SampleBusinessObjectTest.java
+++ test/SampleBusinessObjectTest.java
@@ -1,10 +1,17 @@
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
-
 import org.junit.jupiter.api.Test;
+
+/**
+ * This is the SampleBusinessObject testing class.
+ * @author Shevyn Buhary
+ */
 class SampleBusinessObjectTest {
+    /**
+     * Tester for the getID method for the sampleBusinessObject
+     */
     @Test
-    void test(){
+    void sampleBusinessIDTest(){
         SampleBusinessObject testobj = SampleBusinessObject.createBO(12);
         //assertEquals(testobj.getID(), 12);
         assertThat(testobj.getID(), is(12));
