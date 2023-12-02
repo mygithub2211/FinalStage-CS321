096a1c1c215df8f241c752c402c08b3042f505f2
PHTgitHub <141381198+mygithub2211@users.noreply.github.com>
2023-12-02 17:45:52 -0500

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example {
-    requires javafx.controls;
-    requires javafx.graphics;
-
-    opens com.example to javafx.graphics, javafx.stage, javafx.scene;
-
-    exports com.example;
-}
